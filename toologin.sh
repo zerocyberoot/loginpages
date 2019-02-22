4140259856050c56f7b85e27d9b4246942cc6958
zero cyber <47889422+zerocyberoot@users.noreply.github.com>
2019-02-22 04:01:44 -0800

    Add files via upload

---
+++ toologin.sh
@@ -0,0 +1,130 @@
+#!/system/xbin/bash
+#assalamualaikum WR.WB | ©copyright 2019 edochuii
+#3D0_CHU11
+#tampilanawal (luu recoder,ngk akan jadiin luu master)
+clear
+cd menu
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+#tampilanakhir (luu ubah,ngk akan jadiin luu mastah)
+sleep 1
+echo -e "\033[37;1m{+}\033[32;1m----------------------------------------------\033[37;1m{+}"
+echo -e "\033[33;1m{!}\033[36;1m        W3LCOM3 T0 T00LS LOGIN CREATING\033[33;1m       {!}"
+echo -e "\033[37;1m{+}\033[32;1m----------------------------------------------\033[37;1m{+}"
+echo -e "\033[32;1m | \033[32;1mMader  : 3D0_CHU11/\033[33;1mZ3R0_CYB3R                  \033[32;1m|"                            
+echo -e "\033[32;1m | \033[31;1mKontak : +62895627525979/\033[36;1mzerocyberofficeall    \033[32;1m|"
+echo -e "\033[32;1m | \033[34;1mTeam   : \033[33;1mTH3_TROUBL3_MAK3R                     \033[32;1m|"         
+echo -e "\033[32;1m | \033[36;1mThanks : zerocybersquad \033[31;1m- \033[32;1mTEKAJE 1 Friend \033[32;1m|"
+echo -e "\033[37;1m{+}\033[32;1m----------------------------------------------\033[37;1m{+}"
+echo -e $yellow"{?}             (: SILAHKAN PILIH :)             {?}"
+echo -e "\033[37;1m{+}\033[32;1m----------------------\033[37;1m{+}\033[32;1m---------------------\033[37;1m{+}"
+echo -e $green" |   \033[36;1m[1] \033[32;1mBuat Loginpage   \033[32;1m|   \033[36;1m[4] \033[32;1mHasilnya        \033[32;1m| "
+echo -e $green" |   \033[36;1m[2] \033[33;1mCara pakai       \033[32;1m|   \033[36;1m[5] \033[35;1mAbout           \033[32;1m| "
+echo -e $green" |   \033[36;1m[3] \033[31;1mCara hapus       \033[32;1m|   \033[36;1m[6] \033[31;1mKeluar          \033[32;1m| "
+echo -e "\033[37;1m{+}\033[32;1m----------------------\033[37;1m{+}\033[32;1m---------------------\033[37;1m{+}"
+#codesystem (luu oprek,malah bikin rusak)
+sleep 1
+echo -e $blue
+read -p "<Shut'up~# " chuii
+#kesatu
+if [ $chuii = 1 ] || [ $chuii = 1 ]
+then
+clear
+echo -e $red
+date
+echo -e $green
+python start.py
+cp .login.py $HOME
+sleep 5
+cd $HOME/toologin/
+sh toologin.sh
+fi
+#kedua
+if
+[ $chuii = 2 ] || [ $chuii = 2 ]
+then
+clear
+echo -e $cyan
+sleep 1
+cat carapakai
+fi
+#ketiga
+if [ $chuii = 3 ] || [ $chuii = 3 ]
+then
+clear
+echo -e $red
+sleep 1
+cat carahapus
+fi
+#keempat
+if [ $chuii = 4 ] || [ $chuii = 4 ]
+then
+clear
+sleep 1
+echo -e $white
+cd $HOME/toologin/menu
+python .login.py
+fi
+#kelima
+if [ $chuii = 5 ] || [ $chuii = 5 ]
+then
+clear
+sleep 1
+echo -e "\033[33;1m  ==> LLLL   OOOOO By 3D0_CHU11"
+echo -e "\033[33;1m  ==> L  L  O     O"
+echo -e "\033[33;1m  ==> L  L  O  O  O"
+echo -e "\033[33;1m  ==> L  L  O O O O"
+echo -e "\033[33;1m  ==> L  LLLL  O  O"
+echo -e "\033[33;1m  ==> L      L    O"
+echo -e "\033[33;1m  ==> LLLLLLLLOOOO GIN_Creating==>"
+echo -e $cyan
+cat about.chuii
+echo ""
+fi
+#keenam
+if [ $chuii = 6 ] || [ $chuii = 6 ]
+then
+clear
+echo -e $cyan"TH3_TROUBL3_MAK3R"
+sleep 1
+echo -e $green"CARE ABOUT YOUR SYSTEM"
+sleep 1
+echo -e $green"IF YOU DON'T CARE WE WILL MAKE TROUBLE FOR YOU"
+sleep 1
+echo -e $red"We Are Cyber Jombang"
+sleep 1
+echo -e $cyan"We Are A Maker Trouble"
+sleep 1
+echo -e "\033[00;1mwait. "
+clear
+sleep 1
+echo -e "\033[00;1mwait. . "
+clear
+sleep 1
+echo -e "\033[00;1mwait. . . "
+clear
+sleep 1
+echo -e "\033[00;1mBye:)"
+clear
+exit
+fi
+#rahasia
+if [ $chuii = edo ] || [ $chuii = edo ]
+then
+clear
+echo -e $white"v: Mantaps Jiwa Dan Raga Kami :v"
+pkg install lynx
+lynx facebook.com/edochuii.id
+fi
+if [ $chuii = clear ] || [ $chuii = clear ]
+then
+clear
+cd $HOME/toologin/
+sh toologin.sh
+fi
+#walaikumsallam WR.WB | ©copyright 2019 edochuii
